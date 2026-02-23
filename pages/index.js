3fc3baf7845bf00c6286ea709ff88648b5087779
Lucas Gusson <43628228+lvgusson@users.noreply.github.com>
2026-02-23 16:37:18 +0000

    nova frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home (){
-    return <h1>Larissa Hernandez, minha borboleta! Eu te amo, para toda a minha vida! ❤️</h1>
+    return <h1>Larissa Hernandez, minha borboleta! Eu te amo, para toda a minha vida! ❤️ Se você ver essa mensagem me manda um beijo e um queijo! 🧀</h1>
 }  
 
 export default Home;
