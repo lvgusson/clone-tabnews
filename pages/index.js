bfb3503933af8a86a6ebda4528ea4756ab78879a
Lucas Gusson <43628228+lvgusson@users.noreply.github.com>
2026-02-24 15:47:36 +0000

    prettier configured

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home (){
-    return <h1>Larissa Hernandez, minha borboleta! Eu te amo, para toda a minha vida! ❤️ Se você ver essa mensagem me manda um beijo e um queijo! 🧀</h1>
-}  
+function Home() {
+  return (
+    <h1>
+      Larissa Hernandez, minha borboleta! Eu te amo, para toda a minha vida! ❤️
+      Se você ver essa mensagem me manda um beijo e um queijo! 🧀
+    </h1>
+  );
+}
 
 export default Home;
